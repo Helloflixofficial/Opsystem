61e2c27691c605f4c4a543df4fad6ecc19922f52
Ɓʘɓƴ <shboby71@gmail.com>
2024-07-13 00:17:45 +0530

    work what they do stuf

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,10 @@
-#![no_std] // don't link the Rust standard library
-#![no_main] // disable all Rust-level entry points
-
+#![no_std]
+#![no_main]
 use core::panic::PanicInfo;
-
-#[no_mangle] // don't mangle the name of this function
+#[no_mangle]
 pub extern "C" fn _start() -> ! {
-    // this function is the entry point, since the linker looks for a function
-    // named `_start` by default
     loop {}
 }
-
-/// This function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
