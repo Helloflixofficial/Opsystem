7053907e45bea6079cbce995a69351f80fbb65d7
Ɓʘɓƴ <shboby71@gmail.com>
2024-07-15 23:04:53 +0530

    used valotile for the screen no error

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,3 +1,4 @@
+use volatile::Volatile;
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u8)]
@@ -41,7 +42,7 @@ const BUFFER_WIDTH: usize = 80;
 
 #[repr(transparent)]
 struct Buffer {
-    chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT],
+    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
 }
 pub struct Writer {
     column_position: usize,
@@ -62,10 +63,10 @@ impl Writer {
                 let col = self.column_position;
 
                 let color_code = self.color_code;
-                self.buffer.chars[row][col] = ScreenChar {
+                self.buffer.chars[row][col].write(ScreenChar {
                     ascii_character: byte,
                     color_code,
-                };
+                });
                 self.column_position += 1;
             }
         }
